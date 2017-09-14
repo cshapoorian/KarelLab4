765b4cb6daf6841aa798c1d434f165bbdd1fdb1b
Shapoorian <2006848@student.acalanes.k12.ca.us>
2017-09-14 11:48:11 -0700

    Lab 4 final

--- BeeperBot.java
+++ BeeperBot.java
@@ -7,12 +7,39 @@ import kareltherobot.*;
  */
 public class BeeperBot extends Robot
 {
+    int x = 0;
+    
     public BeeperBot(int st, int av, Direction dir, int numBeepers) {
         super(st, av, dir, numBeepers);
     }
     
     public void runRace() {
+        for (int i = 0; i < 9; i++) {
+           checkRow();
+       }
+    }
+    
+    public void checkRow() {
+        move();
+        turnLeft();
+        while (nextToABeeper()) {
+            pickBeeper();
+            move();
+            x++;
+        }
+        flip();
+        for (int i = x; i > 0; i--) {
+            move();
+        }
+        turnLeft();
+        x = 0;
         
     }
+    
+    public void flip() {
+        turnLeft();
+        turnLeft();
+    }
+    
    
 }
